a352463d3a2dbbc8dcffac640ae6445e9787f896
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-02-03 21:59:36 +0900

    [dev] Topics の view を作った

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,19 @@
+class TopicsController < ApplicationController
+
+  before_action :set_topic, only: [:show]
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+
+  private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+end
