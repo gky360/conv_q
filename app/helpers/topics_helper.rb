3e0184a364c1cee850cb6a9ca6e27c94a10d15e8
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-02-05 23:12:55 +0900

    [#6] トップページに Jumbotron と次の課題ボタン設置
    ついでに _topic.html.erb の改善もした。
    
    resolve #6

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,13 @@
 module TopicsHelper
 
+  def tags_to_label(tags)
+    labels = ""
+    tags.split(",").each do |tag|
+      labels += content_tag :span, tag, class: "label label-default"
+    end
+    return raw labels
+  end
+
   def link_to_source(topic)
     return link_to(topic.source.match(/(http|https):\/\/([^\/]*)/)[2], topic.source)
   end
