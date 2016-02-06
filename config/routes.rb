1153f2fc1aaa744d764b35db49944d40a5c47b92
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-02-07 00:56:22 +0900

    [#9] users#show の作成
    resolve #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
   devise_for :users
   root to: 'conv_q#index'
 
   resources :topics, only: [:index, :show]
+
+  get '/users/:account' => 'users#show', as: 'user'
 
 end
